0d45b33029d312a106ea5368643119278f0f42d5
Stefan Cutovic <scuto26@uw.edu>
2024-05-16 17:57:49 -0700

    Chart 1 Commit - Court Surface Distribution by Tournament Series
    Bar Plot

--- chart1_example.R
+++ chart1_example.R
@@ -1,7 +1,27 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+# Exploratory Analysis, Chart 1
+# Court Surface Distribution by Tournament Series (2013-2023)
+# Stefan Cutovic, Madi Trimmer, Abby Williams
 
+# Loads required libraries to perform visualization
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+library(dplyr)
 
+tennis_data <- read.csv("tennis_atp_3.csv")
+
+# Ensures 'Surface' and 'Series' are treated as categorical variables
+tennis_data$Surface <- factor(tennis_data$Surface, 
+                              levels = c("Clay", "Grass", "Hard"))
+tennis_data$Series <- factor(tennis_data$Series)
+
+# Counts number of matches played on each surface for each series
+surface_counts <- tennis_data %>%
+  group_by(Series, Surface) %>%
+  summarise(Count = n()) %>%
+  ungroup()
+
+# Creates bar plot
+ggplot(surface_counts, aes(x = Series, y = Count, fill = Surface)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Tournament Series", y = "Number of Matches Played", 
+       title = "Court Surface Distribution by Tournament Series (2013 - 2023)") +
+  theme(plot.title = element_text(hjust = 0.5, size = 12, face = "bold"))
